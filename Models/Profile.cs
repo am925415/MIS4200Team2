b46247bb0fb345bf6ae5a9d0912944c0e17243d3
Mackenzie Conroy <mc200015@ohio.edu>
2019-11-13 10:36:51 -0500

    model profile changed

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -2,31 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace MIS4200Team2.Models
 {
-    public class Profile
-    {
-        public int profileID { get; set; }
-        [Display(Name = "First Name")]
-        public string firstName { get; set; }
-        [Display(Name = "Last Name")]
-        public string lastName { get; set; }
-        [Display(Name = "Bio")]
-        public string bio { get; set; }
-        [Display(Name = "Email")]
+        public class Profile
+        {
+            [Display(Name = "First Name:")]
+            [Required(ErrorMessage = "First Name is required")]
+            public string FirstName { get; set; }
+            [Display(Name = "Last Name:")]
+            [Required(ErrorMessage = "Last Name is required")]
+            public string LastName { get; set; }
+            [Display(Name = "Email Address:")]
+            [Required(ErrorMessage = "The email address is required")]
+            [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+            public string Email { get; set; }
+            [Display(Name = "Phone Number:")]
+            [Required(ErrorMessage = "Phone Number is required")]
+            [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+            public string PhoneNumber { get; set; }
 
-        public string email { get; set; }
-        [Display(Name = "Phone Number")]
-        public string phoneNumber { get; set; }
-        [Display(Name = "Employee ID")]
-        public   int employeeID { get; set; }
-        public virtual EmployeeFullDetail EmployeeFullDetail { get; set; }
+            [Display(Name = "Business Unit:")]
+            [Required(ErrorMessage = "Business Unit is required")]
+            public string BusinessUnit { get; set; }
 
+            [Display(Name = "Hire Date:")]
+            [Required(ErrorMessage = "Hire Date is required")]
+            public DateTime HireDate { get; set; }
 
-
-
-}
-}
+            [Display(Name = "State:")]
+            [Required(ErrorMessage = "Please select your state")]
+            public string State { get; set; }
+            public int? State_Id { get; set; }
+            public string State_Name { get; set; }
+            [Display(Name = "Comment:")]
+            public string Comment { get; set; }
+            public string CoutryID { get; set; }
+        }
+        public class EmployeeProfile : DbContext
+        {
+            public DbSet<Profile> EmployeeProfiles { get; set; }
+        }
+    }
